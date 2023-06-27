aca7efc952103db3f153249f12df40f7033e96d9
hoa-bui <hoa.bui@curtin.edu.au>
2023-06-27 22:26:44 +0800

    delete old notebook

--- Notebooks/README.md
+++
@@ -1,5 +0,0 @@
-# Stochastic-Fleet-Composition
-
-## Authors
-1. Hoa Bui
-2. Vinesha Peiris
--- Notebooks/code/main.py
+++
@@ -1,9 +0,0 @@
-
-from algorithms import Algorithms
-from data_gen import DataGeneration
-
-data = DataGeneration(20,200,50,1)
-solution = Algorithms(data)
-
-print(solution.cutting_plane())
-print(solution.linearization())
